d9d455852c4b4f9b8a9586c7f5c87282812d4adb
Sudip Biswas <sudipbiswas9474@gmail.com>
2026-01-03 22:54:48 +0530

    Delete preview directory

--- preview/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/wp-stubs.php';
-
-header('Content-Type: text/html; charset=UTF-8');
-header('Cache-Control: no-cache, no-store, must-revalidate');
-header('Pragma: no-cache');
-header('Expires: 0');
-
-include THEME_DIR . '/front-page.php';
